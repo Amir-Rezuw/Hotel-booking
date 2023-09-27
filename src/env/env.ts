9a3d61c49909ad87419097cd5ed6345066cb1252
AmirReza Jamali <amir7jamali.ar@yahoo.com>
2023-09-27 14:35:13 +0330

    api key removed

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,5 +1,4 @@
 export const env = {
   baseUrl: "http://localhost:5000/",
   GeocodingBaseUrl: "https://api.bigdatacloud.net/data/reverse-geocode-client",
-  __GEOCODING_ACCESS_TOKEN: "AIzaSyAm2vZ3016MzECr3aUrk5Qthky_aL4Vh7I",
 };
